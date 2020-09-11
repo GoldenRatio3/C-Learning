37ee8156e9df2e6519858ec6cc32897c4cee7526
Brad Winter <bradleyswinter@hotmail.co.uk>
2020-09-11 18:53:26 +0100

    Added null pointer to pointers example

--- pointersExample.c
+++ pointersExample.c
@@ -4,6 +4,7 @@ int main () {
 
   int var1 = 10;
   int *intPointer; /* pointer variable declartion */
+  int *ptr = NULL; /* example null pointer */
 
   intPointer = &var1; /* store address of var1 in pointer variable */
 
@@ -13,6 +14,14 @@ int main () {
  
   printf("Value of *intPointer variable is %d\n", *intPointer);
 
+  /* to check if a pointer is null or not */
+  if (ptr) {
+    printf("The value of ptr is not null %x\n", ptr);
+  } 
+
+  if (!ptr) {
+    printf("the value of ptr is null %x\n", ptr);
+  }
   return 0;
 }
 
