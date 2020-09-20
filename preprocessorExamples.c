3a10e622c9c58669850f471f2fcb90dd3cd7e2e2
Brad Winter <bradleyswinter@hotmail.co.uk>
2020-09-20 13:12:07 +0100

    Added preprocess example

--- preprocessorExamples.c
+++ preprocessorExamples.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
+#define message_for(a, b) \
+  printf(#a " and " #b ": Happy B day!\n");
+
 int main() {
   printf("File: %s\n", __FILE__);
   printf("Date: %s\n", __DATE__);
   printf("Time: %s\n", __TIME__);
   printf("Line: %d\n", __LINE__);
   
+  message_for(Brad, Aliza);
+
   return 0;
 }
 
