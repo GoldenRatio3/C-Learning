355d157ad6d4b24174d847bd32ec4ca133b33512
Brad Winter <bradleyswinter@hotmail.co.uk>
2020-09-20 17:38:56 +0100

    Added token pasting to preprocessor example

--- preprocessorExamples.c
+++ preprocessorExamples.c
@@ -3,6 +3,8 @@
 #define message_for(a, b) \
   printf(#a " and " #b ": Happy B day!\n");
 
+#define tokenpaster(n) printf("token" #n " = %d", token##n);
+
 int main() {
   printf("File: %s\n", __FILE__);
   printf("Date: %s\n", __DATE__);
@@ -10,6 +12,9 @@ int main() {
   printf("Line: %d\n", __LINE__);
   
   message_for(Brad, Aliza);
+
+  int token34 = 40;
+  tokenpaster(34);
 
   return 0;
 }
